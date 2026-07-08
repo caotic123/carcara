438143cb60cd8acf4dca050db957f513fccd031f
Tiago Campos <tiagocampos@mixrank.com>
2026-07-08 19:46:50 -0300

    Rollback rules for empty operators

--- carcara/src/rare/mod.rs
+++ carcara/src/rare/mod.rs
@@ -33,6 +33,8 @@ pub fn get_rules() -> Vec<(RewriteTerm, RewriteTerm)> {
         build_equation!((Or x) ~> x),
         build_equation!((Or true) ~> true),
         build_equation!((And false) ~> false),
+        build_equation!((Or) ~> true),
+        build_equation!((And) ~> false),
         // Arithmetic
         build_equation!((Add (RareList ..x..)) ~> (Add x)),
         build_equation!((Add x) ~> x),
